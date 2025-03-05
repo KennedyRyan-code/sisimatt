c0c85d392c3767b59f31d4fc0fc6f46710669096
KennedyRyan-code <kenryan49@gmail.com>
2025-03-05 16:46:36 +0300

    update Home component to fetch and display products and categories; fix search form action URL

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+import ProductsView from "@/components/ProductsView";
+import { getAllCategories } from "@/sanity/lib/getAllCategories";
+import { getAllProducts } from "@/sanity/lib/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+  const categories = await getAllCategories();
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <Button>Click me</Button>
+      <div className="flex flex-col justify-between justify-top min-h-screen bg-gray-100 p-4">
+        <h1 className="text-2xl font-bold">All Products</h1>
+        <ProductsView products={products} categories={categories} />
+      </div>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -27,13 +27,13 @@ function Header() {
             <Link href='/' className='text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0'>
             Sisimatt
             </Link>
-            <Form action='/serach' className='sm:w-auto sm:mx-4 mt-2 sm:mt-0'>
+            <Form action='/search' className='sm:w-auto sm:mx-4 mt-2 sm:mt-0'>
                 <input type='text'
                 name='query'
                 placeholder='Search for products'
                 className='bg-gray-100 test-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focuse:ring-blue-500 focus:ring-opacity-50 bprder w-full max-w-4xl'
                 />
-                <button type='submit'>Search</button>
+                <button type='submit' className=' '>Search</button>
             </Form>
             <div className='flex items-center space-x-4 mt-4 sm:mt-0'>
                <Link href='/cart' className='flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 text-white font-bold bg-blue-500 hover:bg-blue-700 hover:opacity-50  py-2 px-4  rounded cursor-pointer'>
@@ -65,7 +65,7 @@ function Header() {
                         ) : (
                             <SignInButton mode='modal' />
                         )}
-
+                        {/* passkey fuctionality */}
                         {user?.passkeys.length === 0 && (
                             <button
                                 onClick={createClerkPasskey}
