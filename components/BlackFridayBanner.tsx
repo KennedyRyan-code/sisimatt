c4dd3f471193d9f1ced9031447f0d33e2afed5fd
KennedyRyan-code <kenryan49@gmail.com>
2025-03-08 19:58:45 +0300

    enhance Black Friday banner with dynamic sale details and improved layout

--- components/BlackFridayBanner.tsx
+++ components/BlackFridayBanner.tsx
@@ -5,9 +5,27 @@ async function BlackFridayBanner() {
   if(!sale?.isActive) return null;
   
   return (
-    <div className="bg-gradient-to-r from-red-600 to-black text-white p-10 py-16 mx-6 mt-4 rounded-lg shadow-lg">
-      BlackFridayBanner
+    <div className="bg-gradient-to-r from-red-600 to-black text-white p-10 py-10 mx-6 mt-4 rounded-lg shadow-lg">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex-1">
+          <h2 className="text-3xl sm:text-5xl font-extrabold text-left mb-4">
+          {sale.title}
+          </h2>
+          <p className="text-left text-lg sm:text-xl font-semibold mb-6">
+            {sale.description}
+          </p>
+
+          <div className="flex">
+            <div className="bg-white text-black py-4 px-6 rounded-full shadow-md transform hover:scale-105 translate duration-300">
+              <span className="font-bold text-base sm:text-lg">Use code:{" "}</span>
+              <span className="text-red-600">{sale.couponCode}</span>
+              <span className="ml-2 font-bold text-base sm:text-xl">for {sale.discountAmount}% OFF</span>
+
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
   )
 }
 
